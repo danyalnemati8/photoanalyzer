702665dc21b5b50f599f6b9eab141c1ad2a4eeaa
danyalnemati8 <70175019+danyalnemati8@users.noreply.github.com>
2023-07-31 14:43:17 -0700

    added the key

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, redirect, url_for, session, request
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'  # Replace with a secret key for security
+app.secret_key = 'IGQVJVQVpvNWZAtdkQwQlIteFNqUnQ0M1BqVUEzTzVxZA1B5WWhDNDZA2Qjk4bTJXNzhiazYxcnVDZAmh2eE9FTmFLdnZA0U1NKcmtxQ3VialgxMmpmR09pNXo4SzMyZA3JqSjBJRWcwek1TQ3hXRXlTWlZAVaQZDZD'  # Replace with a secret key for security
 
 # Dummy data for demonstration purposes
 dummy_photos = [
@@ -24,17 +24,32 @@ dummy_photos = [
     # Add more dummy photos here...
 ]
 
+# Replace this with actual API integration to fetch photos using the access token
+def fetch_user_photos(access_token):
+    # In a real app, use the access_token to make API requests to fetch user photos
+    # For this example, we'll return the dummy photos
+    return dummy_photos
+
 @app.route('/')
 def index():
-    if 'access_token' not in session:
+    access_token = session.get('access_token')
+    if not access_token:
         return redirect(url_for('login'))
-    return render_template('homepage.html', photos=dummy_photos)
+
+    # Fetch user's photos using the access token (Replace this with actual API calls)
+    user_photos = fetch_user_photos(access_token)
+
+    return render_template('homepage.html', photos=user_photos)
 
 @app.route('/login')
 def login():
     # In a real app, you would implement Facebook Login here
     # For this example, we'll just set a dummy access token
-    session['access_token'] = 'dummy_access_token'
+    dummy_access_token = 'dummy_access_token'
+    
+    # Store the access token in the user's session
+    session['access_token'] = dummy_access_token
+    
     return redirect(url_for('index'))
 
 @app.route('/logout')
